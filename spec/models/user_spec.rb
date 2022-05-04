42f1497bb7c4dfa301276d103f7bd454bcb6adca
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-04 22:29:02 +0900

    add models spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject { user } 
+    let(:user) { build(:user, name: name, email: email) }
+
+    context "when name is blank" do
+      let(:user) { build(:user, name: '') }
+
+      it do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "when email is blank" do
+      let(:user) { build(:user, email: '') }
+
+      it do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context "when email is not unique" do
+      before { create(:user, email: 'test@example.com') }
+
+      let(:user) { build(:user, email: 'test@example.com') }
+      
+      it do
+        expect(subject).not_to be_valid
+      end
+    end
+  end
 end
