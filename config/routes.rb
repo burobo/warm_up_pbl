47928ed2d68bb59b3b62e2352f2a7cd1a7e38a53
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-04 20:28:16 +0900

    ログイン、ログアウトボタン設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
   devise_for :users
-  get 'pages/index'
   get 'pages/detail'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
