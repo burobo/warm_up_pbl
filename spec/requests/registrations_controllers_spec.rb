e8536a4e67cd3db80085e358048a1942b44af81e
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-07 16:38:08 +0900

    ログインのテストを追加

--- spec/requests/registrations_controllers_spec.rb
+++ spec/requests/registrations_controllers_spec.rb
@@ -8,13 +8,43 @@ RSpec.describe "Registrations", type: :request do
 
     it do
       expect { subject }.to change(User, :count).by(1)
-      expect(response).to have_http_status("302")
+      expect(response).to redirect_to root_url
     end
 
     context "when invalid" do
       let(:params) { attributes_for(:user, password: '') }
       it do
         expect { subject }.not_to change(User, :count)
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe "POST /users/sign_in" do
+    subject { post user_session_path, params: { user: params } }
+    let(:current_user) { create(:user) }
+    let(:params) do
+      {
+        email: current_user.email,
+        password: current_user.password
+      }
+    end
+
+    it do
+      subject
+      expect(response).to redirect_to root_url
+    end
+
+    context "when invalid" do
+      let(:params) do
+        {
+          email: current_user.email,
+          password: 'invalid'
+        }
+      end
+
+      it do
+        subject
         expect(response).to have_http_status(:success)
       end
     end
