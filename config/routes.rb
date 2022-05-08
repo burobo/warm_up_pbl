cbfd5766945d6fb6564e3d24d3caf96e5030d138
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-08 12:04:22 +0900

    sns認証機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'pages/detail'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
