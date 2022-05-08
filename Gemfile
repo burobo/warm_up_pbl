e0b4265f829660e1ec4a457ef3fef3ee1d13a477
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-08 21:16:58 +0900

    google認証実装（メールアドレス登録）

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'dotenv-rails'
 gem 'omniauth-facebook'
 gem 'omniauth-twitter'
 gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,11 +15,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect root_path, event: :authentication
+      sign_in(:user, @user)
+      redirect_to root_path
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_path
     end
   end
 
