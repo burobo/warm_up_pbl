83d655820e14a0405fef98278d5779fb8f420cb2
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-10 19:04:11 +0900

    SNS認証時にメールアドレス情報がなければ自動生成するよう設定（メールアドレス必須のため）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2 github]
 
   validates :uid, uniqueness: { scope: :provider }
 
@@ -11,8 +11,12 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
-      user.email = auth.info.email
+      auth.info.email ? user.email = auth.info.email : user.email = User.dummy_email(auth)
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
   end
 end
