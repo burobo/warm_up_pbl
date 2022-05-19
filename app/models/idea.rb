5e4b74cdda501a949d3653302a148cd3b3b76ece
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-05-20 08:18:29 +0900

    アイデア詳細画面でいいねしたユーザー情報を閲覧できるよう関連付けを追加

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,5 @@ class Idea < ApplicationRecord
   validates :title, :outline, :detail, :user, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
 end
