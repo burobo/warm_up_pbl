08b97fd6cb4dff3a4b98dbf0e000c8dcaf96a2b0
burobo <boronobu@gmail.com>
2022-06-05 04:01:10 +0900

    通知の表示機能を作成した(未デザイン反映)

--- spec/requests/notifications_request_spec.rb
+++ spec/requests/notifications_request_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
-
+  let(:user) { create(:user) }
+  
+  describe "GET /notifications" do
+    context "user is authenticated" do
+      before do
+        sign_in user
+      end
+      it "returns http success" do
+        get '/notifications'
+        expect(response).to have_http_status(:success)
+      end
+    end
+    context "user is not authenticated" do
+      it "redirect to sign in page" do
+        get '/notifications'
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(:new_user_session)
+      end
+    end
+  end
 end
