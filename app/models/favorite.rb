5a1e88ebea7b1acbe1ddb14984639e84ff31ef99
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-06-05 10:21:40 +0900

    いいね時に通知を作成する（解除時は削除）

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :idea
-  has_one :notification
+  has_one :notification, dependent: :destroy
   validates_uniqueness_of :idea_id, scope: :user_id
 end
