5989b1f384e1a22e27964551a12253933eac96e5
burobo <boronobu@gmail.com>
2022-06-13 21:04:45 +0900

    文法ミスを修正

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-unless [nil, "test"].include?(ENV["RAILS_ENV"]) do
+unless [nil, "test"].include?(ENV["RAILS_ENV"]) then
   creds = Aws::Credentials.new(Rails.application.credentials[:aws][:access_key_id], Rails.application.credentials[:aws][:secret_access_key])
 
   Aws::Rails.add_action_mailer_delivery_method(
