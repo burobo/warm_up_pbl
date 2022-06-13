0f82a678c529229aeb398a6e4dc5313753a38b29
Kota Saito <a2117ks@aiit.ac.jp>
2022-06-13 22:31:33 +0900

    改行コード対応

--- app/javascript/packs/markdown_editor.js
+++ app/javascript/packs/markdown_editor.js
@@ -6,9 +6,6 @@ window.onload = function(){
     uploadImage: true,
     imagePathAbsolute: true,
     imageUploadEndpoint: '任意のエンドポイント',
-    showIcons: ['strikethrough', 'code', 'table', 'redo', 'heading', 'undo', 'heading-bigger', 'heading-smaller', 'heading-1', 'heading-2', 'heading-3', 'clean-block', 'horizontal-rule'],
     element: document.getElementById('idea_detail')
   });
-  
-  console.log(document.getElementById('idea_detail'));
 };
