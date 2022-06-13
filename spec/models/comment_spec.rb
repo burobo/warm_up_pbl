3069fca6c050f0598f3b72ffd2eb85f617074613
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-06-13 11:15:05 +0900

    add model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject { comment }
+    let(:comment) { build(:comment, content: content) }
+
+    context "when content is blank" do
+      let(:comment) { build(:comment, content: '') }
+
+      it do
+        expect(subject).not_to be_valid
+      end
+    end
+  end
 end
