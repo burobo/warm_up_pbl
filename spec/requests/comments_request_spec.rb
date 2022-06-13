9730c9040fb81fccd3347e3af653c29cdfe2d356
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-06-13 12:05:16 +0900

    add request spec

--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  let(:user) { create :user }
 
+  describe 'POST /ideas/:ideas_show_id/comments' do
+    before do
+      sign_in user
+    end
+    subject { post ideas_show_comments_path(idea), params: params, headers: headers }
+
+    let(:idea) { create :idea }
+    let(:headers) { { HTTP_REFERER: ideas_show_url(idea) } }
+    let(:params) do
+      {
+        comment: {
+          content: 'hogehoge',
+          idea_id: idea.id
+        }
+      }
+    end
+
+    it do
+      expect { subject }.to change(Comment, :count).by(1)
+      expect(response).to redirect_to headers[:HTTP_REFERER]
+    end
+  end
 end
