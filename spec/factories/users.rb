ba83598c8b55fa54dd869af0b582d086df495da1
Kota Saito <a2117ks@aiit.ac.jp>
2022-06-20 21:06:19 +0900

    FactoryBotのカラム追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 6) }
+    confirmed_at { Date.today }
   end
 end
