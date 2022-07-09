f99cdb26734d0157d8ca50bdc86f10fe13d389b5
Kota Saito <a2117ks@aiit.ac.jp>
2022-07-09 14:11:38 +0900

    メール文章修正

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationMailer < ActionMailer::Base
   def idea_was_favorited
     @notification = params[:notification]
-    mail(to: @notification.user.email, from: "info@aiit-idea-farm.net", subject: "あなたのアイデアが見せて！されました！【#{@notification.favorite.idea.title}】")
+    mail(to: @notification.user.email, from: "info@aiit-idea-farm.net", subject: "あなたのアイデアが見せて！されました。【#{@notification.favorite.idea.title}】")
   end
 end
