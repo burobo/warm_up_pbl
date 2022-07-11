8a4a3fe82079bffaf26fd5f967ea4ee574383e68
Kota Saito <a2117ks@aiit.ac.jp>
2022-07-11 20:50:18 +0900

    絵文字データをサーバー内部に保持する

--- app/javascript/packs/emoji_button.js
+++ app/javascript/packs/emoji_button.js
@@ -1,10 +1,14 @@
 import { createPicker } from 'picmo';
+import emojiData from 'emojibase-data/en/data.json';
+import messages from 'emojibase-data/en/messages.json';
 
 window.addEventListener('load', function() {
   // emojipicker生成
   const container = document.querySelector('.pickerContainer');
   const picker = createPicker({
-    rootElement: container
+    rootElement: container,
+    emojiData,
+    messages
   });
 
   //選択された絵文字をセットする
