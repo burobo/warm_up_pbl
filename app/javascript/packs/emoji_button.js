36e48868c3aa453eac6bd29b023a334051d65a37
Kota Saito <a2117ks@aiit.ac.jp>
2022-07-11 21:30:07 +0900

    アイデア編集画面開いた時、既存の絵文字をピッカーで選択する

--- app/javascript/packs/emoji_button.js
+++ app/javascript/packs/emoji_button.js
@@ -3,12 +3,15 @@ import emojiData from 'emojibase-data/en/data.json';
 import messages from 'emojibase-data/en/messages.json';
 
 window.addEventListener("turbolinks:load", function() {
+  // 既存の絵文字取得
+  const initEmoji = document.getElementById('init_emoji').value;
   // emojipicker生成
   const container = document.querySelector('.pickerContainer');
   const picker = createPicker({
     rootElement: container,
     emojiData,
-    messages
+    messages,
+    initialEmoji:initEmoji
   });
 
   //選択された絵文字をセットする
