7820150b5dbf178f86627f8d85706510ef4b646a
Kota Saito <a2117ks@aiit.ac.jp>
2022-07-11 09:12:38 +0900

    アイデア作成画面に絵文字追加

--- app/javascript/packs/emoji_button.js
+++ app/javascript/packs/emoji_button.js
@@ -1,8 +1,14 @@
 import { createPicker } from 'picmo';
 
 window.onload = function () {
+  // emojipicker生成
   const container = document.querySelector('.pickerContainer');
   const picker = createPicker({
     rootElement: container
+  });
+
+  //選択された絵文字をセットする
+  picker.addEventListener('emoji:select', event => {
+    document.getElementById('emoji_field').value = event.emoji;
   });
 };
