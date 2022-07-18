ac750f5efb51ea01d10f243e60c81f69271f4765
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-07-18 23:05:43 +0900

    OGP画像を適切なサイズにリサイズ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
         description: :description, 
         type: 'website',
         url: request.original_url,
-        image: image_url("sprout.png"),
+        image: image_url("ogp_sprout.png"),
         locale: 'ja_JP',
       },
       fb: {
