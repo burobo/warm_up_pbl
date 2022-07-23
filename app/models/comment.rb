8f0ade3f1887484cefb65b5d8b50230e19969e39
burobo <boronobu@gmail.com>
2022-07-23 11:18:11 +0900

    コメント通知のテーブル定義に依存するファイルを変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :idea
+  has_one :notification, dependent: :destroy
   validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_074313) do
+ActiveRecord::Schema.define(version: 2022_07_23_014712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2022_07_17_074313) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "comment_id"
+    t.index ["comment_id"], name: "index_notifications_on_comment_id"
     t.index ["favorite_id"], name: "index_notifications_on_favorite_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
@@ -110,6 +112,7 @@ ActiveRecord::Schema.define(version: 2022_07_17_074313) do
   add_foreign_key "favorites", "ideas"
   add_foreign_key "favorites", "users"
   add_foreign_key "ideas", "users"
+  add_foreign_key "notifications", "comments"
   add_foreign_key "notifications", "favorites"
   add_foreign_key "notifications", "users"
 end
