bffd170c843789e023c3d660e832af3edc5c1706
burobo <boronobu@gmail.com>
2022-07-23 11:35:41 +0900

    コメント時に通知を保存する処理を作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,14 @@ class CommentsController < ApplicationController
     comment = current_user.comments.new(comments_params)
     if idea.user == current_user || current_user == idea.users.find_by(id: current_user.id)
       if comment.save
+        comment.create_notification_comment
         redirect_to request.referer, notice: "コメントを投稿しました。"
       else
         redirect_to request.referer, notice: "コメントの投稿に失敗しました。"
       end
     end
+
+
   end
   
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@ class Comment < ApplicationRecord
   belongs_to :idea
   has_one :notification, dependent: :destroy
   validates :content, presence: true
+  
+  def create_notification_comment()
+    Notification.create!(
+      event: Notification.events[:commented],
+      comment_id: self.id,
+      user_id: self.idea.user.id
+    )
+  end
 end
