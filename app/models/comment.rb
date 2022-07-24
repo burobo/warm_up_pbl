fe43ac6750f1e12fc8aeee2c00f512b3987ad757
burobo <boronobu@gmail.com>
2022-07-24 13:44:37 +0900

    他のイイネしたユーザへの通知を作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,5 +10,21 @@ class Comment < ApplicationRecord
       comment_id: self.id,
       user_id: self.idea.user.id
     )
+    
+    notifications_for_other_users_favorite = self.idea.favorites.select {
+        |favorite| favorite.user_id != self.user.id
+      }.map{
+        |favorite| {
+          event: Notification.events[:commented],
+          comment_id: self.id,
+          user_id: favorite.user_id,
+          created_at: Time.current,
+          updated_at: Time.current
+        }
+      }
+
+      unless notifications_for_other_users_favorite.blank?
+        Notification.insert_all!(notifications_for_other_users_favorite)
+      end
   end
 end
